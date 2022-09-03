6dae054b0bdc5d75214bcf7e8026707548db295b
namansharma3007 <namansharma3007c@gmail.com>
2022-09-03 12:56:38 +0530

    comparison strings

---
+++ Comparison.java
@@ -0,0 +1,22 @@
+// Strings 
+// only for value use .equals() method
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a = "Naman";
+        String b = "Naman";
+
+        // == 
+        // System.out.println(a == b);
+
+        // explicitely creating new objects
+
+        String name1 = new String("Varsha");
+        String name2 = new String("Varsha");
+
+        // System.out.println(name1 == name2);
+        // System.out.println(name1.equals(name2));
+
+        System.out.println(name1.charAt(0));
+    }
+}
