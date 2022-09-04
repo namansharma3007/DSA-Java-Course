977f2cb0d3800664883d34c7b42257a18d436a04
namansharma3007 <namansharma3007c@gmail.com>
2022-09-04 22:00:20 +0530

    notes

---
+++ PreetyPrinting.java
@@ -0,0 +1,16 @@
+public class PreetyPrinting {
+    public static void main(String[] args) {
+        float a = 482.56684130f;
+        // will round the value as well
+        System.out.printf("Formatted number value is: %.2f",a);
+        System.out.println();
+        System.out.println(Math.PI);
+
+        System.out.printf("Pi: %.3f",Math.PI);
+        System.out.println();
+
+
+        System.out.printf("My name is %s and I study at %s.","Naman","Lovely Professional University");
+
+    }
+}
