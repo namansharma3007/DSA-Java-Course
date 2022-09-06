2e2358b168ea4e53548b2cd811e3f8060141d01e
namansharma3007 <namansharma3007c@gmail.com>
2022-09-06 18:57:07 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -1,10 +1,13 @@
+import javax.sound.sampled.spi.FormatConversionProvider;
+
 public class Pattern {
     public static void main(String[] args) {
         int n = 5;
         // pattern1(n);
         // pattern2(n);
         // pattern3(n);
-        pattern4(n);
+        // pattern4(n);
+        pattern5(n);
     }
 
     static void pattern1(int n){
@@ -40,6 +43,29 @@ public class Pattern {
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <=row; col++) {
                 System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        // for (int row = 1; row <= n; row++) {
+        //     for (int col = 1; col <= row; col++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int row = n - 1; row >= 1; row--) {
+        //     for (int col = row; col >= 1; col--) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        for (int row = 1; row <= 2*n; row++) {
+            int totalColsInRow = row > n ? 2*n - row : row;
+            for (int col = 1; col <= totalColsInRow ; col++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
