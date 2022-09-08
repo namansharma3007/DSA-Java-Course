80689d34b5e98a9bac9251d4015aed0b86632f5b
namansharma3007 <namansharma3007c@gmail.com>
2022-09-08 22:25:36 +0530

    changing names

--- swap.java
+++ swap.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 public class swap {
     public static void main(String[] args) {
         int[] arr = { 1, 5, 3, 6, 9, 10 };
-        swap(arr, 1, 4);
+        swap1(arr, 1, 4);
 
         System.out.println(Arrays.toString(arr));
         reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void swap(int[] arr, int index1, int index2) {
+    static void swap1(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
@@ -20,7 +20,7 @@ public class swap {
         int start = 0;
         int end = arr.length - 1;
         while(start<end) {
-            swap(arr, start, end);
+            swap1(arr, start, end);
             start++;
             end--;
         }
