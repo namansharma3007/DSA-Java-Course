54d428cc6a1fe2bb64ffe9f9065c6ec7dc6373fc
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-10-04 00:16:16 +0530

    fibonacci formula

---
+++ Fibonacci_no.java
@@ -0,0 +1,24 @@
+public class Fibonacci_no {
+
+    static int fibo(int n){
+        if (n < 2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+
+    static int fiboFormula(int n){
+        // just for demo use long insted
+    //    return (int)((Math.pow(((1 + Math.sqrt(5)) / 2),n) - Math.pow(((1 - Math.sqrt(5)) / 2),n)) / Math.sqrt(5));
+       return (int)(Math.pow(((1 + Math.sqrt(5)) / 2),n) / Math.sqrt(5)); // removing the less dominating terms
+    }
+    public static void main(String[] args) {
+        // int no = 5;
+        // System.out.println(fibo(no));
+
+        // for(int i=0;i<11;i++){
+        //     System.out.println(fiboFormula(i));
+        // }
+        System.out.println(fiboFormula(50));
+    }
+}
