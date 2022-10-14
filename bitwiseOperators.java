3e55c59eafaeb0a51999b8553e7e7d597ab1204b
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-10-15 02:12:59 +0530

    bitwise

--- bitwiseOperators.java
+++ bitwiseOperators.java
@@ -13,11 +13,29 @@ class bitwiseOperators {
         return unique;
     }
 
+    // find ith bit of that no
+    static int ithBit(int i,int no){
+        return no & (1<<i-1);
+    }
+    // doubt
+    static void setIthBitTo1(int i,int no){
+        no = no | (1<<i-1);
+    }
+
+    static int rightMostBit(int no){
+
+        return 0;
+    }
     public static void main(String[] args) {       
         // System.out.println(oddEven(45));
 
         int [] arr = {1,2,3,4,5,2,3,4,5,6,7,6};
 
-        System.out.println(appearingOnce(arr));
+        // System.out.println(appearingOnce(arr));
+
+        // System.out.println(ithBit(1,5));
+        int no = 9;
+        setIthBitTo1(1, no);
+        // System.out.println(no);
     }
 }
