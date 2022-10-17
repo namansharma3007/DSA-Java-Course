17939387cc966c7ad8363d37324bb29ff157f8d5
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-10-17 14:56:45 +0530

    resursion

--- recursionArrays.java
+++ recursionArrays.java
@@ -1,7 +1,9 @@
 public class recursionArrays {
     public static void main(String[] args) {
         int [] arr = {1,2,3,5,6,8,4};
-        System.out.println(checkIfSorted(arr));
+        // System.out.println(checkIfSorted(arr));
+        System.out.println(searchItem(arr, 4, 0));
+
     }
 
     // find if array is sorted or not
@@ -14,5 +16,13 @@ public class recursionArrays {
         if(index == arr.length) return true;
 
         return (arr[index] > arr[index-1] && helperSorted(arr, index+1));
+    }
+
+    // find an item in arr
+    static int searchItem(int [] arr,int target,int index){
+        if(arr[index] == target) return index;
+        if(index == arr.length-1) return -1;
+
+        return searchItem(arr, target, index+1);
     }
 }
