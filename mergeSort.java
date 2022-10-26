159ddb7cb2cb856d5ed28d53f8072dbaec860011
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-10-26 12:05:35 +0530

    mwege sort

--- mergeSort.java
+++ mergeSort.java
@@ -4,8 +4,10 @@ public class mergeSort {
     // divide array into two parts then get both the parts sorted then merge both
     public static void main(String[] args) {
         int [] arr = {5,4,3,2,1};
-        arr = mSort(arr);
+        // arr = mSort(arr);
         // int [] ans = mSort(arr); // Now, try printing original array, it would not have been modified
+        // System.out.println(Arrays.toString(arr));
+        mergSortInPlace(arr, 0, arr.length);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -53,5 +55,53 @@ public class mergeSort {
         }
 
         return mix;
+    }
+
+     static void mergSortInPlace(int [] arr, int start, int end){
+        if((end - start) == 1){
+            return;
+        }
+
+        int mid =(start + end)/2;
+        mergSortInPlace(arr, start, mid);
+        mergSortInPlace(arr, mid, end);
+
+        mergeInPlace(arr, start, mid, end);
+    }
+
+    private static void mergeInPlace(int[] arr, int start, int mid, int end) {
+        int [] mix = new int[end - start];
+
+        int i = start;
+        int j = mid;
+        int k = 0;
+
+        while(i < mid && j < end){
+            if(arr[i] < arr[j]){
+                mix[k] = arr[k];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        // it may be case that one of  the arrays is not complete
+        // copy the remaining elements
+        while(i < mid){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        
+        while(j < end){
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[start+l] = mix[l]; 
+        }
     }
 }
