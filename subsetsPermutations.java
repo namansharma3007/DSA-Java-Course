a04c55443fe96f033e7d5a1e13d08283f85891f5
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-11-03 17:02:35 +0530

    permutations

---
+++ subsetsPermutations.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class subsetsPermutations {
+    public static void main(String[] args) {
+        // permutations("", "abc");
+        ArrayList<String> ans = permutationsReturn("", "abc");
+        System.out.println(ans);
+    }
+
+    static void permutations(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        for (int i = 0; i <= p.length(); i++) {
+            String first = p.substring(0, i);
+            String second = p.substring(i,p.length());
+            permutations(first + ch + second, up.substring(1));
+        }
+
+    }
+
+    static ArrayList<String> permutationsReturn(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+            String first = p.substring(0, i);
+            String second = p.substring(i,p.length());
+            ans.addAll(permutationsReturn(first + ch + second, up.substring(1)));
+        }
+
+        return ans;
+
+    }
+}
