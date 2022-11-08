b894fcb86d96eb7fd867092c860428b6bfbc86fe
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-11-08 13:50:03 +0530

    n queen problem

---
+++ nQueensProblem.java
@@ -0,0 +1,12 @@
+public class nQueensProblem {
+    public static void main(String[] args) {
+        int n = 4;
+        int queens = 4;
+
+        int [][] board = new int [n][n];
+
+    }
+
+    
+    
+}
