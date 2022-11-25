62adc3ac314ad062978101f47014655edbc280c2
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-11-25 15:52:28 +0530

    first commit

--- bitwiseOperators.java
+++ bitwiseOperators.java
@@ -26,26 +26,79 @@ class bitwiseOperators {
 
         return 0;
     }
-    private static int findSingleNo(int[] num) {
+    public static int findSingleNo(int[] num) {
         int no = 0;
         for (int i = 0; i < num.length; i++) {
             no+=num[i];
         }
         return no%3;
     }
+
+    private static int magicNo(int n) {
+
+        int ans = 0;
+
+        int base = 5;
+        while(n > 0){
+            int last = n & 1;
+            n = n>>1;
+            ans+=last * base;
+            base*=5;
+        }
+
+        return ans;
+    }
+    public static int noOfBitsInBinaryRepresentation(int i) {
+        return (int)(Math.log(i)/Math.log(2))+1;
+    }
+    public static int sumInAnyRowInPascalsTriangle(int i){
+        return 1 << (i-1);
+    }
+    public static boolean powerOfTwo(int n) {
+        if(n==0) return false;
+        return (n&(n-1)) == 0;
+    }
+
+    public static int aRaiseToPowerb(int a, int power){
+
+        int ans = 1;
+        while (power > 0) {
+            if ((power&1) == 1) {
+                ans*=a;
+            }
+
+            a*=a;
+            power = power >> 1;
+        }
+        return ans;
+    }
     public static void main(String[] args) {       
         // System.out.println(oddEven(45));
 
-        int [] arr = {1,2,3,4,5,2,3,4,5,6,7,6};
+        // int [] arr = {1,2,3,4,5,2,3,4,5,6,7,6};
 
         // System.out.println(appearingOnce(arr));
 
         // System.out.println(ithBit(1,5));
-        int no = 9;
-        setIthBitTo1(1, no);
+        // int no = 9;
+        // setIthBitTo1(1, no);
         // System.out.println(no);
-        int [] num = {1,1,1,2,3,3,3,4,4,4,5,5,5};
-        System.out.println(findSingleNo(num));
+        // int [] num = {1,1,1,2,3,3,3,4,4,4,5,5,5};
+        // System.out.println(findSingleNo(num));
+
+        // System.out.println(magicNo(6));
+
+        // System.out.println(noOfBitsInBinaryRepresentation(5));
+
+        //  System.out.println(sumInAnyRowInPascalsTriangle(2));
+
+        // System.out.println(powerOfTwo(16));
+
+        // System.out.println(aRaiseToPowerb(3,3));
+
     }
+
+
+
 
 }
