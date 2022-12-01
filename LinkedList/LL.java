de0d27beda7cf3c4fe31cafd5f810aa780983be2
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2022-12-01 12:58:24 +0530

    reversal of linked list

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -174,6 +174,81 @@ public class LL {
         
         return ans;
     }
+    public void bubbleSort() {
+        bubbleSort(size - 1, 0);
+    }
+
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
+
+    public void reverse(){
+        reverse(head);
+    }
+
+    private void reverse(Node node) {
+        if(node == tail){
+            head = tail;
+            return;
+        }
+
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+
+    private void inplaceReversal(Node head){
+        if(size < 2){
+            return;
+        }
+
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+
+        while(present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next != null){
+                next = next.next;
+
+            }
+        }
+        head = prev;
+    }
+
     private class Node {
         private int value;
         private Node next;
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -3,29 +3,29 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(3);
-        list.insertFirst(4);
-        list.insertFirst(5);
-        list.insertFirst(6);
+        // LL list = new LL();
+        // list.insertFirst(3);
+        // list.insertFirst(4);
+        // list.insertFirst(5);
+        // list.insertFirst(6);
 
-        list.insertLast(99);
+        // list.insertLast(99);
 
-        list.insert(45, 3);
+        // list.insert(45, 3);
 
-        list.display();
-        System.out.println(list.deleteFirst());
+        // list.display();
+        // System.out.println(list.deleteFirst());
 
-        list.display();
+        // list.display();
 
-        System.out.println(list.deleteLast());
-        list.display();
+        // System.out.println(list.deleteLast());
+        // list.display();
 
-        System.out.println(list.delete(2));
-        list.display();
+        // System.out.println(list.delete(2));
+        // list.display();
 
-        list.insertRec(88, 1);
-        list.display();
+        // list.insertRec(88, 1);
+        // list.display();
 
         // DoublyLL list = new DoublyLL();
         // list.insertFirst(3);
@@ -51,7 +51,13 @@ public class Main {
 
 
 
-
+        LL list = new LL();
+        for (int i = 7; i > 0; i--) {
+            list.insertLast(i);
+        }
+        list.display();
+        list.bubbleSort();
+        list.display();
        
     }
 }
