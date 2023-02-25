3a8fa8e0661462f592865f1c4f53d0ddad28c2b2
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-02-25 09:48:18 +0530

    static study

--- OOPS/staticExample/Main.java
+++ OOPS/staticExample/Main.java
@@ -1,22 +1,39 @@
 package OOPS.staticExample;
 
-
 public class Main{
     public static void main(String[] args) {
-        Human naman = new Human(20, "Naman Sharma", 1000, false);
-        Human rahul = new Human(34, "Rahul Raja", 2000, true);
-        Human arpit = new Human(35, "Arpit Kumar", 3000, true);
+        // Human naman = new Human(20, "Naman Sharma", 1000, false);
+        // Human rahul = new Human(34, "Rahul Raja", 2000, true);
+        // Human arpit = new Human(35, "Arpit Kumar", 3000, true);
+        
+        // // Wrong convention but will still work
+        // System.out.println(rahul.population);
+        // System.out.println(naman.population);
+        // System.out.println(arpit.population);
+
+        // // Correct convention it must be acessed with class variable
+        // // Above one will also work but is not the correct method for working
+        // System.out.println(Human.population);
         
 
-        // Wrong convention but will still work
-        System.out.println(rahul.population);
-        System.out.println(naman.population);
-        System.out.println(arpit.population);
+    }
+    // which is not static belongs to an object
+    void greeting(){
+        System.out.println("Hello world");
+        fun();
+    }
 
-        // Correct convention it must be acessed with class variable
-        // Above one will also work but is not the correct method for working
-        System.out.println(Human.population);
-
+    static void fun(){
+        // you can'y use this because it requires an instance
+        // but the function you are using it in does not depends on instances
+        // this will give you  an error
+        // greeting();
+        
+        
+        // you cannot access non-static stuff wothout referencing their instances in a static context
+        // hence, here I an referencing
+        Main obj = new Main();
+        obj.greeting();
 
     }
 }
