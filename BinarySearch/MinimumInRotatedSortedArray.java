29a87cd5a6e5965d025d43e88cc988882467a8a3
Naman Sharma <namansharma3007c@gmail.com>
2024-04-03 18:51:52 +0530

    new chanegs

--- BinarySearch/MinimumInRotatedSortedArray.java
+++ BinarySearch/MinimumInRotatedSortedArray.java
@@ -3,15 +3,16 @@ package BinarySearch;
 public class MinimumInRotatedSortedArray {
     static int minimumNumberUniqueArray(int [] arr, int n){
         int start = 0, end = n-1;
-        int min = -1;
+        int min = Integer.MAX_VALUE;
         while(start <= end){
             int mid = start + (end + start)/2;
 
             if(arr[mid] >= arr[start]){
-                min = arr[start];
+                min = Math.min(min, arr[start]);
                 start = mid+1;
             } else {
-                end = mid;
+                min = Math.min(min, arr[mid]);
+                end = mid-1;
             }
         }
         return min;
