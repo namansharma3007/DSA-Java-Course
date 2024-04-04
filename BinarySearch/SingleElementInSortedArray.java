d9b64c79d5eae37e7e978f6a542e4a896ec10e10
Naman Sharma <namansharma3007c@gmail.com>
2024-04-04 13:58:13 +0530

    single element in sorted array

---
+++ BinarySearch/SingleElementInSortedArray.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+import java.util.*;
+
+public class SingleElementInSortedArray {
+    public static int singleNonDuplicate(ArrayList<Integer> arr)
+    {
+        int n = arr.size();
+
+        if(n == 1) return arr.get(0);
+
+        if(!arr.get(0).equals(arr.get(1))) return arr.get(0);
+
+        if(!arr.get(n-1).equals(arr.get(n-2))) return arr.get(n-1);
+
+        int start = 1;
+        int end = n-2;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(!arr.get(mid).equals(arr.get(mid+1)) && !arr.get(mid).equals(arr.get(mid-1))){
+                return arr.get(mid);
+            }
+            if((mid % 2 == 1 && arr.get(mid).equals(arr.get(mid-1))) 
+            || (mid % 2 == 0 && arr.get(mid).equals(arr.get(mid+1)))){
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+}
