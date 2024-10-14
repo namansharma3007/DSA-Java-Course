2f58b379c3f52ccd298ec2acd744c18effd7e54e
Naman Sharma <namansharma3007c@gmail.com>
2024-10-14 11:32:26 +0530

    changes

--- .CP/Solution.java
+++ .CP/Solution.java
@@ -6,6 +6,19 @@ public class Solution {
     public static final FastWriter fw = new FastWriter();
     final static int mod = (int) 1e9 + 7;
 
+    public static long power(long base, long exp) {
+        long result = 1;
+        base = base % mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+
     // Fast input class for efficient reading of input
     static class FastReader {
         BufferedReader br;
